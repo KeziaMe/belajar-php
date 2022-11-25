12cbc0527b12f98949b016f1d10e7c39fe20d92e
Kezia Megawati <keziamega13@gmail.com>
2022-11-25 22:49:32 +0700

    membuat fungsi create dan read data

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+// koneksi dg mysql
+$con = mysqli_connect("localhost","root","","fakultas");
+// cek koneksi
+if (mysqli_connect_errno()){
+    echo "koneksi gagal".mysqli_connect_error();
+}else{
+    echo "koneksi berhasil";
+}
+
+// membaca data dari table mysql
+$query = "select*from mahasiswa";
+
+// tampilkan data dg menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result){
+    // $row = mysqli_fetch_assoc($result);
+    // var_dump($row);
+    // echo $row["nama"];
+    // echo $row["alamat"];
+
+    while($row = mysqli_fetch_assoc($result)){
+        // echo "<br>".$row["nama"];
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+}
+
+// tutup koneksi mysql
+// mysqli_close($con);
+
+// foreach($mahasiswa as $value){
+//     echo $value["nama"];
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+</table>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,61 @@
+<?php
+
+// tangkap data dari form submit
+ if (isset($_POST["submit"])){
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $id_jurusan = $_POST['id_jurusan'];
+    $tempat_lahir = $_POST['tempat_lahir'];
+    $tgl_lahir = $_POST['tgl_lahir'];
+    $jk = $_POST['jk'];
+    $alamat = $_POST['alamat'];
+
+    // koneksi dg mysql
+    $con = mysqli_connect("localhost","root","","fakultas");
+    // cek koneksi
+    if (mysqli_connect_errno()){
+        echo "koneksi gagal".mysqli_connect_error();
+    }else{
+        echo "koneksi berhasil";
+    }
+
+
+    //  buat sql query untuk insert ke database
+    // buat query insert dan jalankan
+    $sql = "insert into mahasiswa (id_jurusan, nim, nama, jk, tempat_lahir, tgl_lahir, alamat)
+            values ($id_jurusan,'$nim','$nama','$jk','$tempat_lahir','$tgl_lahir', '$alamat')";
+
+    // jalankan query
+    if (mysqli_query($con, $sql)){
+        echo "Data berhasil ditambah";
+    }else{
+        echo "Ada error ". mysqli_error();
+    }
+
+    mysqli_close($con);
+ }
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+    <form action="" method="post">
+        NIM: <input type="text" name="nim"><br>
+        Nama: <input type="text" name="nama"><br>
+        Id Jurusan: <input type="number" name="id_jurusan"><br>
+        Jenis Kelamin: <input type="text" name="jk"><br>
+        Tempat Lahir: <input type="text" name="tempat_lahir"><br>
+        Tanggal Lahir (yyyy-mm-dd): <input type="text" name="tgl_lahir"><br>
+        Alamat: <input type="text" name="alamat"><br>
+        <button type="submit" name="submit">Tambah Data</button>
+    </form>
+</body>
+</html>
